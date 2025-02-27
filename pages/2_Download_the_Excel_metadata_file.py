7a8674f9dd9064f5b64e2dae090f4b4ca0f19e10
Nukorn Plainpan <127328032+NukP@users.noreply.github.com>
2025-02-27 15:42:34 +0100

    Update Excel files

--- pages/2_Download_the_Excel_metadata_file.py
+++ pages/2_Download_the_Excel_metadata_file.py
@@ -6,9 +6,9 @@ markdown_content = """
 Here you will find an Excel file template that you can fill out with your metadata
 We will add more template for other cell types in the future. Be sure to check out!
 ### Blank Excel metadata file
-[Coin cell battery schema version 1.0.0](https://github.com/user-attachments/files/18182511/241125_Battery2030%2B_CoinCellBattery_Schema_Ontologized_1.0.0_blank.xlsx) 
+[Coin cell battery schema version 1.0.0]((https://github.com/user-attachments/files/19011068/241125_Battery2030%2B_CoinCellBattery_Schema_Ontologized_1.0.0_blank.xlsx) 
 ### Example filled out Excel metadata file
-[Example-filled Coin cell battery schema version 1.0.0](https://github.com/user-attachments/files/18182585/241125_Battery2030%2B_CoinCellBattery_Schema_Ontologized_1.0.0_filled.xlsx)
+[Example-filled Coin cell battery schema version 1.0.0](https://github.com/user-attachments/files/19011086/241125_Battery2030%2B_CoinCellBattery_Schema_Ontologized_1.0.0_filled.xlsx)
 
 
 """
