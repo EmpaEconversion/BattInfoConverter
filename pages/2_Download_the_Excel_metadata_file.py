fafea164d78228190b3975a01073627f545118f1
Nukorn Plainpan <127328032+NukP@users.noreply.github.com>
2025-06-03 16:17:52 +0200

    Update 2_Download_the_Excel_metadata_file.py
    Update the download link

--- pages/2_Download_the_Excel_metadata_file.py
+++ pages/2_Download_the_Excel_metadata_file.py
@@ -5,11 +5,15 @@ markdown_content = """
 ## Excel metadata files
 Here you will find an Excel file template that you can fill out with your metadata
 We will add more template for other cell types in the future. Be sure to check out!
+
+Please note that the empty file cannot be submitted to conversion directly as the blank file lacks 4 required fields: CellID, Date of cell assembly, Institution/company and Scientist/technician/operator.
+These fields are needed to be filled before the file can be subjected to the conversion. 
+
+
 ### Blank Excel metadata file
-[Coin cell battery schema version 1.0.0](https://github.com/user-attachments/files/19011470/241125_Battery2030%2B_CoinCellBattery_Schema_Ontologized_1.0.0_blank.xlsx) 
+[Coin cell battery schema version 1.1.2](https://github.com/EmpaEconversion/BattInfoConverter/raw/main/Excel%20for%20reference/250515_241125_Battery2030%2B_CoinCellBattery_Schema_Ontologized_1.1.2_filled.xlsx) 
 ### Example filled out Excel metadata file
-[Example-filled Coin cell battery schema version 1.0.0](https://raw.githubusercontent.com/EmpaEconversion/BattInfoConverter/main/Excel%20for%20reference/241125_Battery2030%2B_CoinCellBattery_Schema_Ontologized_1.0.0_filled.xlsx
-)
+[Example-filled Coin cell battery schema version 1.1.2](https://github.com/EmpaEconversion/BattInfoConverter/raw/main/Excel%20for%20reference/250515_241125_Battery2030%2B_CoinCellBattery_Schema_Ontologized_1.1.2_filled.xlsx)
 
 
 """
