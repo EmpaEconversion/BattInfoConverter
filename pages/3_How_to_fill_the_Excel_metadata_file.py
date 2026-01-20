4e2127b939bb7ff0717137248eec7d77989aed4e
Nukorn Plainpan <nukorn.p27@gmail.com>
2026-01-20 13:47:35 +0100

    Apply the change in the tab name of Excel schema into the app description

--- pages/3_How_to_fill_the_Excel_metadata_file.py
+++ pages/3_How_to_fill_the_Excel_metadata_file.py
@@ -5,15 +5,15 @@ markdown_content = """
 
 - All the tabs present in the Excel file are essential for the web app. **Do not change their names**.
 - The first row of each tab contains the column names. These are also used by the web app. **Do not change their names**.
-- To fill in the metadata using our template file, simply fill in the data in the `Schema` tab and `Unique ID` tab.
+- To fill in the metadata using our template file, simply fill in the data in the `@Schema` tab and `@Classes` tab.
 
-### Schema Tab
+### @Schema Tab
 This tab contains the majority of the metadata file.
 - **Value**: Enter the metadata value. If the cell is empty, the script will skip this metadata item.
 - **Unit**: Specify the unit of the metadata. If the metadata item doesn't require a unit, enter "No Unit." Leaving this cell blank will result in an error.
 - **Ontology Link**: Provide the ontology link. If you do not want to ontologize a particular row, enter "NotOntologize." To add a comment instead, enter "Comment."
 
-### Unique ID Tab
+### @Classes Tab
 All unique string values (not numbers) are highly recommended to come with their own persistent unique identifier (online persistent unique identifier).
 This includes the names of researchers or institutions. Please note that the chemical name itself is not unique.
 
@@ -32,7 +32,7 @@ __2) The item is listed in the "Item" column but its respective unique ID is not
 - Some items are ontologized but lack a unique ID. For example, "R2032," which is the name of a coin cell case, is listed in the BattInfo ontology but does not have a unique ID.
 - The app will add this value in "@type" in the resulting JSON-LD file.
 
-__3) The item is not listed at all in the Unique ID tab__
+__3) The item is not listed at all in the @Classes tab__
 - This is for **Items with no Ontology link and no unique ID**.
 - Some items lack a unique ID, such as the name of a newly-synthesized compound.
 - The app will add this value in `rdfs:comment` in the resulting JSON-LD file.
@@ -41,4 +41,3 @@ __3) The item is not listed at all in the Unique ID tab__
 #####################################################################
 
 st.markdown(markdown_content, unsafe_allow_html=True)
-
--- pages/4_Adapting_for_custom_data.py
+++ pages/4_Adapting_for_custom_data.py
@@ -5,8 +5,8 @@ markdown_content = """
 
 If you have your custom data schemas (you would like to change what to include in your metadata), you can simply modify our Excel metadata file to suit your custom data.
 
-- You can add or remove existing rows in the `Schema` tab to suit your data schemas.
-- When new rows are added to the `Schema` tab, please ensure to follow these steps:
+- You can add or remove existing rows in the `@Schema` tab to suit your data schemas.
+- When new rows are added to the `@Schema` tab, please ensure to follow these steps:
 
 #### 1. **Adding New Rows in the Metadata Column**
 - The values in `Metadata`, `Type`, `Priority`, and `Comment` are not used by the web app and are therefore optional. You can fill in any name and information that best helps you manage the metadata file.
@@ -22,24 +22,23 @@ If you have your custom data schemas (you would like to change what to include i
     - If you are using BattInfo as your ontology concept, Dr. Simon Clark (simon.clark@sintef.no) will be delighted to assist you.
 
 #### 3. **Adding Any Additional Ontology Top-Level**
-- Sometimes, the appropriate ontology concept is not listed in the BattInfo ontology. You can use ontology concepts from other providers, such as `schema.org`. If this is the case, simply list your top-level ontology domain in the `@context-TopLevel` tab.
+- Sometimes, the appropriate ontology concept is not listed in the BattInfo ontology. You can use ontology concepts from other providers, such as `schema.org`. If this is the case, simply list your top-level ontology domain in the `@Context` tab.
 - List the ontology shorthand and its respective link in the `Item` tab and `Key` tab, respectively.
 - You can represent these ontology concepts from other sites using the notation `Ontology shorthand: Ontology concept`.
   - For example, the `comment` ontology is listed at https://www.w3.org/TR/rdf-schema/#ch_comment. We give `rdfs` as its shorthand. So, this can be represented as `rdfs:comment`.
 
 #### 4. **List the Connector**
-- Each of the ontology items in the `Ontology link` column in the `Schema` tab that **is not the last one** is considered a connector and must be listed in the `@context-Connector` tab.
+- Each of the ontology items in the `Ontology link` column in the `@Schema` tab that **is not the last one** is considered a connector and must be listed in the `@Predicates` tab.
 - The items listed in the `Key` column are the "Default @type" for the listed ontology term. If the "Default @type" is listed, this type will be included in `"@type"` every time such a connector is used. This is optional, and not every connector has this "Default @type". For example:
   - `hasBinder`: has a default type of "Binder". Every time `hasBinder` is used, "Binder" will be added to `"@type"` of `hasBinder`.
   - `hasProperty`: has no default type.
 
 #### 5. **Adding New Units**
-- Add the unit in the `Unit` column in the `Schema` tab.
-- In the `Ontology - Unit` tab, define the new unit. Include the `Item`, `Label`, and `Symbol` columns, along with the ontology reference in the `Key` column.
+- Add the unit in the `Unit` column in the `@Schema` tab.
+- In the `@Units` tab, define the new unit. Include the `Item`, `Label`, and `Symbol` columns, along with the ontology reference in the `Key` column.
 
 """
 #####################################################################
 
 st.markdown(markdown_content, unsafe_allow_html=True)
-
 
