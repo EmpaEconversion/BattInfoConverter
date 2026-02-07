44612505d36bbaf5251074b0d4e4f674b8547cd7
Nukorn Plainpan <127328032+NukP@users.noreply.github.com>
2026-02-07 23:08:32 +0100

    Update Excel metadata template links to version 1.1.15

--- pages/2_Download_the_Excel_metadata_file.py
+++ pages/2_Download_the_Excel_metadata_file.py
@@ -11,9 +11,9 @@ These fields must be filled before the file can be sbmitted for conversion.
 
 
 ### Blank Excel metadata template file
-[Coin cell battery template version 1.1.14](https://github.com/EmpaEconversion/BattInfoConverter/raw/refs/heads/development/Excel%20for%20reference//BattINFO_converter_standard_Excel_version_1.1.14_empty.xlsx) 
+[Coin cell battery template version 1.1.15](https://github.com/EmpaEconversion/BattInfoConverter/raw/refs/heads/development/Excel%20for%20reference//BattINFOconverter_CoinCell_template_version_1.1.15_empty.xlsx) 
 ### Example filled Excel metadata template file
-[Example-filled coin cell battery template version 1.1.14](https://github.com/EmpaEconversion/BattInfoConverter/raw/refs/heads/development/Excel%20for%20reference/BattINFO_converter_standard_Excel_version_1.1.14_filled.xlsx)
+[Example-filled coin cell battery template version 1.1.15](https://github.com/EmpaEconversion/BattInfoConverter/raw/refs/heads/development/Excel%20for%20reference/BattINFOconverter_CoinCell_template_version_1.1.15_filled.xlsx)
 
 
 """
