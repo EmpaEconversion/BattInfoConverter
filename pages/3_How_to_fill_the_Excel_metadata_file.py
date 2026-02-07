b9d3101ec600acd5d64ba4fdb97a6885959c0786
Nukorn Plainpan <nukorn.p27@gmail.com>
2026-02-07 19:55:25 +0100

    change some writing

--- pages/3_How_to_fill_the_Excel_metadata_file.py
+++ pages/3_How_to_fill_the_Excel_metadata_file.py
@@ -1,20 +1,20 @@
 import streamlit as st
 
 markdown_content = """
-# How to Fill Out the Excel Metadata File
+# How to fill the Excel metadata file
 
 - All the tabs present in the Excel file are essential for the web app. **Do not change their names**.
 - The first row of each tab contains the column names. These are also used by the web app. **Do not change their names**.
-- To fill in the metadata using our template file, simply fill in the data in the `@Schema` tab and `@Classes` tab.
+- To fill in the metadata using our template file, simply fill in the data in the `@Schema` tab. Additional ontology terms can be added in the @Predicates and `@Classes` tab as needed.
 
 ### @Schema Tab
 This tab contains the majority of the metadata file.
 - **Value**: Enter the metadata value. If the cell is empty, the script will skip this metadata item.
-- **Unit**: Specify the unit of the metadata. If the metadata item doesn't require a unit, enter "No Unit." Leaving this cell blank will result in an error.
+- **Unit**: Specify the unit of the metadata. If the metadata item does not require a unit, enter "No Unit." Leaving this cell blank will result in an error.
 - **Ontology Link**: Provide the ontology link. If you do not want to ontologize a particular row, enter "NotOntologize." To add a comment instead, enter "Comment."
 
 ### @Classes Tab
-All unique string values (not numbers) are highly recommended to come with their own persistent unique identifier (online persistent unique identifier).
+All unique string values (not numbers) are highly recommended to come with their own persistent online unique resource identifier (online persistent unique identifier).
 This includes the names of researchers or institutions. Please note that the chemical name itself is not unique.
 
 For instance, Aluminium used in different battery cells is the same material by name but refers to distinct pieces.
@@ -29,7 +29,7 @@ __1) The item is listed in the "Item" column and its respective unique ID is lis
 
 __2) The item is listed in the "Item" column but its respective unique ID is not listed in the "ID" column__
 - This is for **Items with an Ontologized link but no unique ID**.
-- Some items are ontologized but lack a unique ID. For example, "R2032," which is the name of a coin cell case, is listed in the BattInfo ontology but does not have a unique ID.
+- Some items are ontologized but lack a unique ID. For example, "R2032," which is the format of a coin cell case, is listed in the BattInfo ontology but does not have a unique ID.
 - The app will add this value in "@type" in the resulting JSON-LD file.
 
 __3) The item is not listed at all in the @Classes tab__
