8736fc61807be445c4285a1db644b719d3dd28e6
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-04-27 14:15:54 +0200

    Update download page

--- pages/2_Download_the_Excel_metadata_file.py
+++ pages/2_Download_the_Excel_metadata_file.py
@@ -1,18 +1,49 @@
+"""Downloads for excel templates."""
+
+import json
+from io import BytesIO
+from pathlib import Path
+
 import streamlit as st
 
-markdown_content = """
+from battinfoconverter_backend.templates.template_conversion import COINCELL_TEMPLATE_PATH, dict_to_workbook
 
-## Excel metadata files
-Here you will find Excel templates that you can fill out with your metadata.
-We will add more template for other cell types in the future. Be sure to check them out!
 
-Please note that the empty file cannot be submitted to conversion directly as the blank file lacks 4 required fields: CellID, Date of cell assembly, Institution/company and Scientist/technician/operator.
-These fields must be filled before the file can be sbmitted for conversion. 
+@st.cache_data
+def get_xlsx_bytes(schema_path: Path, *, empty: bool) -> tuple[bytes, str]:
+    """Create xlsx bytes object, and get version."""
+    with schema_path.open("r") as f:
+        data = json.load(f)
+    version = next(
+        (
+            c["Value"]
+            for c in data["@Schema"]["data"]["Cell identification"]["rows"]
+            if c["Metadata"] == "Schema version"
+        ),
+        "0.0.0",
+    )
+    wb = dict_to_workbook(data, empty=empty)
+    buffer = BytesIO()
+    wb.save(buffer)
+    buffer.seek(0)
+    return buffer.getvalue(), version
 
-### Blank Excel metadata template file
-[Coin cell battery template version 1.1.16](https://github.com/EmpaEconversion/BattInfoConverter/raw/refs/heads/main/Excel%20for%20reference/BattINFO_converter_standard_Excel_version_1.1.16_empty.xlsx)
-### Example filled Excel metadata template file
-[Example-filled coin cell battery template version 1.1.16](https://github.com/EmpaEconversion/BattInfoConverter/raw/refs/heads/main/Excel%20for%20reference/BattINFO_converter_standard_Excel_version_1.1.16_filled.xlsx)
-"""
 
-st.markdown(markdown_content, unsafe_allow_html=True)
+xlsx_bytes, version = get_xlsx_bytes(COINCELL_TEMPLATE_PATH, empty=False)
+xlsx_bytes_empty, _ = get_xlsx_bytes(COINCELL_TEMPLATE_PATH, empty=True)
+
+st.title("Download the Excel metadata file")
+st.text("Here you will find Excel templates that you can fill out with your metadata.")
+st.subheader("Excel template files")
+st.download_button(
+    label=f"⬇️ Coin cell battery template v{version} - empty",
+    data=xlsx_bytes_empty,
+    file_name=f"BattINFO_converter_standard_Excel_version_{version}_empty.xlsx",
+    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+)
+st.download_button(
+    label=f"⬇️ Coin cell battery template v{version} - filled example",
+    data=xlsx_bytes,
+    file_name=f"BattINFO_converter_standard_Excel_version_{version}_filled.xlsx",
+    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+)
