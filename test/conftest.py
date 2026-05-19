51ef59f27424659436fe00345eab8b3e3054147a
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-05-19 17:42:06 +0200

    Patch pending terms into tests

--- test/conftest.py
+++ test/conftest.py
@@ -14,6 +14,7 @@ from battinfoconverter_backend.templates.template_conversion import (
     ELECTROLYSIS_TEMPLATE_PATH,
     FLOWCELL_TEMPLATE_PATH,
 )
+from battinfoconverter_backend.validate import get_context
 
 jsonld.set_document_loader(jsonld.requests_document_loader())
 
@@ -109,6 +110,20 @@ def coincell(request: pytest.FixtureRequest) -> CellFixtures:
 def old_schema(request: pytest.FixtureRequest) -> Path:
     """Get path to old xlsx templates."""
     return Path(REF_DIR / request.param)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def add_pending_terms() -> None:
+    """Add pending terms to context for validation."""
+    ctx = get_context()  # populates global _MAPPED_TERMS
+    ctx["https://w3id.org/emmo/domain/battery#"] += [
+        "ImmersionElectrode",
+        "NegativeElectrodeGasCompartment",
+        "PolyArylPiperidinium",
+        "PositiveElectrodeGasCompartment",
+        "hasSubstrate",
+        "pH",
+    ]
 
 
 def coerce_decimals(value: Decimal | float | dict | list) -> float | dict | list:
