cf5ffee7233ea7bc6524ac256106580f9c26a039
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-07-03 17:26:15 +0200

    Ensure utf-8 encoding on update expected

--- scripts/update_expected.py
+++ scripts/update_expected.py
@@ -19,5 +19,5 @@ for template in Path("src/battinfoconverter_backend/templates").glob("*.json"):
 
     json_to_xlsx(template, excel_path)
     res = convert_excel_to_jsonld(excel_path)
-    with output_path.open("w") as f:
+    with output_path.open("w", encoding="utf-8") as f:
         json.dump(res, f, indent=4, ensure_ascii=False)
