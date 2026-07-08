11cdb33e424a21d3fc0d75a9f0907b4371e4ca65
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-07-08 16:12:15 +0200

    Add RFB to catinfo downloads

--- catinfo_app/pages/2_Download_Excel_templates.py
+++ catinfo_app/pages/2_Download_Excel_templates.py
@@ -9,6 +9,7 @@ import streamlit as st
 from battinfoconverter_backend.templates.template_conversion import (
     COINCELL_TEMPLATE_PATH,
     ELECTROLYSIS_TEMPLATE_PATH,
+    FLOWCELL_TEMPLATE_PATH,
     dict_to_workbook,
 )
 
@@ -35,6 +36,9 @@ def get_xlsx_bytes(schema_path: Path, *, empty: bool) -> tuple[bytes, str]:
 
 coincell_xlsx_bytes, coincell_version = get_xlsx_bytes(COINCELL_TEMPLATE_PATH, empty=False)
 coincell_xlsx_bytes_empty, _ = get_xlsx_bytes(COINCELL_TEMPLATE_PATH, empty=True)
+
+flowcell_xlsx_bytes, flowcell_version = get_xlsx_bytes(FLOWCELL_TEMPLATE_PATH, empty=False)
+flowcell_xlsx_bytes_empty, _ = get_xlsx_bytes(FLOWCELL_TEMPLATE_PATH, empty=True)
 
 electrolysis_xlsx_bytes, electrolysis_version = get_xlsx_bytes(ELECTROLYSIS_TEMPLATE_PATH, empty=False)
 electrolysis_xlsx_bytes_empty, _ = get_xlsx_bytes(ELECTROLYSIS_TEMPLATE_PATH, empty=True)
@@ -67,5 +71,19 @@ st.download_button(
     label=f"⬇️ Coin cell battery template v{coincell_version} - filled example",
     data=coincell_xlsx_bytes,
     file_name=f"BattINFO_coincell_v{coincell_version}_filled.xlsx",
+    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+)
+
+st.subheader("Redox flow battery cell")
+st.download_button(
+    label=f"⬇️ Redox flow cell template v{flowcell_version} - empty",
+    data=flowcell_xlsx_bytes_empty,
+    file_name=f"BattINFO_flowcell_v{flowcell_version}_empty.xlsx",
+    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+)
+st.download_button(
+    label=f"⬇️ Redox flow cell template v{flowcell_version} - filled example",
+    data=flowcell_xlsx_bytes,
+    file_name=f"BattINFO_flowcell_v{flowcell_version}_filled.xlsx",
     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
 )
